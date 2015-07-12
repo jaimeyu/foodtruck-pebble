fd3d6862eaf6f68a68ba87c9a5f66082c1a68ce8
Jaime Yu <jaime@jaimeyu.com>
2015-07-12 15:01:55 +0000

    Removed some more dead commented code. Sorted the result list according to the closest place to you.

--- src/app.js
+++ src/app.js
@@ -9,12 +9,14 @@ var Vector2 = require('vector2');
 var ajax = require('ajax');
 
 
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);  
 var main = new UI.Card({
-	title: 'Pebble.js',
+	title: 'Food Trucks near you',
 	icon: 'images/menu_icon.png',
-	subtitle: 'Hello World!',
-	body: 'Press any button.'
+	subtitle: 'Loading...',
+	body: 'Grabbing data...'
 });
+	get_truck_data();
 
 main.show();
 main.on('click', 'up', function(e) {
@@ -54,16 +56,21 @@ var locationOptions = {
 
 function locationSuccess(pos) {
 	console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+	/* Set it to local storage. What's the performance hit? */
+	localStorage.setItem("my_lat", pos.coords.latitude);
+	localStorage.setItem("my_long", pos.coords.longitude);
+
 }
 
 function locationError(err) {
 	console.log('location error (' + err.code + '): ' + err.message);
 }
 
+
+
 // Make an asynchronous request
 
-function parse_truck_data(response){
-	navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);  
+  function parse_truck_data(response){
 	/* 
 	   This is where the hackathon is happening. 
 	   lat= 39.0437 lon= -77.4875 
@@ -75,17 +82,22 @@ function parse_truck_data(response){
 	console.log("Current time now is: ", cur_time);
 
 	var o = response.vendors;
+	/* Get all the known vendors */
 	Object.keys(o).forEach(function (key) {
 
-		//For some reason, we time out if we do too many ops.
+		/* For some reason, we time out if we do too many ops.
+		 * Limit to only ~10 items for now...
+		 */
 		if (cnt > 9) { return;}
 
 		cnt++;
 		var t = o[key];
-		console.log(t.name);
-		//console.log("Hey this works?",(o[key]).name);
 
 		var co = t.open;
+		/* For each vendor,
+		 * 	check if they have a truck open RIGHT NOW
+		 * 	and push into the result_list
+	 	 */
 		Object.keys(co).forEach(function (key) {
 			//console.log("Is it open?");
 			var op = co[key];
@@ -98,18 +110,46 @@ function parse_truck_data(response){
 						subtitle:op.display + op.special + t.description,
 						where: op.display,
 						why: op.special,
-						description: t.description
+						description: t.description,
+						latitude: t.latitude,
+						longitude: t.longitude
 					});
 			}
 		});
 	});
 
+	/* Order the list by closest to you */
+	list_results.sort( function(a,b) {
+		var magnitude_a = a.latitude + a.longitude;
+		var magnitude_b = b.latitude + b.longitude;
+		
+	
+		var my_lat = localStorage.getItem("my_lat");
+		var my_long = localStorage.getItem("my_long");
+
+		var my_mag = my_lat + my_long;
+
+		var diff_a = my_mag - magnitude_a;
+		var diff_b = my_mag - magnitude_b;
+
+		if ( diff_a > diff_b ) {
+			return 1;
+		} else if (diff_a < diff_b ) {
+			return -1;
+		} 
+		return 0;
+		
+	});
+
+
+	/* Show the menu list on the pebble */
 	var menu = new UI.Menu({
 		sections: [{
 			title: 'Open Trucks',
 			items: list_results
 		}]
 	});
+	/* When you click a menu item, show a card with more info */
 	menu.on('select', function(e) {
 		console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
 		console.log('The item is titled "' + e.item.title + '"');
@@ -121,9 +161,9 @@ function parse_truck_data(response){
 		card.show();
 
 	});
+	
 	menu.show();
 	console.log("Done parsing the trucks");
-	return list_results;
 
 }
 
